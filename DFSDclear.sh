0f837491d8a24db0b1203abaf250a464b51d1908
pSzlufik <95447079+pSzlufik@users.noreply.github.com>
2024-10-06 22:25:25 +0200

    Update DFSDclear.sh
    Debug messages added for lib

--- DFSDclear.sh
+++ DFSDclear.sh
@@ -177,28 +177,37 @@ xdotool mousemove --window "$WID" 1 1 sleep 0.2 click --window "$WID" --repeat 2
 xdotool mousemove --window "$WID" 590 990 sleep 0.2 click --window "$WID" 1 sleep 0.1
 xdotool mousemove --window "$WID" 1 1 sleep 0.2 click --window "$WID" --repeat 2 --delay 0.05 1
 fsPattern=$(($1 % 3))
+echo "Input: $1 $2 $3"
 let fsNode1=$2-1
 let fsNode2=$3-1
 temp=$(getMatrixElement "$fsPattern" "$fsNode1" 16 "${fsTree[@]}")
 IFS=';' read -r fsCol1 fsRow1 <<< "$temp"
 fsCol1Index=$((fsCol1-1))
 fsRow1Index=$((fsRow1-1))
+echo "Tree pattern: $fsPattern"
+echo "Upgrade indexes: $fsNode1 $fsNode2"
+echo "First matrix value: $temp"
 temp=$(getMatrixElement "$fsPattern" "$fsNode2" 16 "${fsTree[@]}")
+echo "Second matrix value: $temp"
 IFS=';' read -r fsCol2 fsRow2 <<< "$temp"
 fsCol2Index=$((fsCol2-1))
 fsRow2Index=$((fsRow2-1))
+echo "Col/Row for first upagrde: $fsCol1 / $fsRow1"
+echo "Col/Row for second upagrde: $fsCol2 / $fsRow2"
+t1=$(max $(($fsCol1-6)) 0)
+t2=$(max $(($fsCol2-6)) 0)
+echo "Shifting multiplier first: $t1"
+echo "Shifting multiplier second: $t2"
 
 xdotool mousemove --window "$WID" 1 1 sleep 0.2 click --window "$WID" --repeat 120 --delay 1 4 sleep 0.1
 xdotool mousemove --window "$WID" 1 1 sleep 0.2 click --window "$WID" --repeat "$(getSingleRowElement "$fsCol1Index" "${fsScrollsPerColumn[@]}")" --delay 1 5 sleep 0.1
-t=$(max $(($fsCol1-6)) 0)
-xdotool mousemove --window "$WID" "$((20+t*460))" "$(getSingleRowElement "$fsRow1Index" "${fsRows[@]}")" sleep 0.2 click --window "$WID" 1 sleep 0.1
+xdotool mousemove --window "$WID" "$((20 + $t1 * 460))" "$(getSingleRowElement "$fsRow1Index" "${fsRows[@]}")" sleep 0.2 click --window "$WID" 1 sleep 0.1
 xdotool mousemove --window "$WID" 750 790 sleep 0.2 click --window "$WID" 1 sleep 0.1
 xdotool mousemove --window "$WID" 20 20 sleep 0.2 click --window "$WID" --repeat 2 --delay 250 1 sleep 0.1
 
 xdotool mousemove --window "$WID" 1 1 sleep 0.2 click --window "$WID" --repeat 120 --delay 1 4 sleep 0.1
 xdotool mousemove --window "$WID" 1 1 sleep 0.2 click --window "$WID" --repeat "$(getSingleRowElement "$fsCol2Index" "${fsScrollsPerColumn[@]}")" --delay 1 5 sleep 0.1
-t=$(max $(($fsCol2-6)) 0)
-xdotool mousemove --window "$WID" "$((20+t*460))" "$(getSingleRowElement "$fsRow2Index" "${fsRows[@]}")" sleep 0.2 click --window "$WID" 1 sleep 0.1
+xdotool mousemove --window "$WID" "$((20 + $t2 * 460))" "$(getSingleRowElement "$fsRow2Index" "${fsRows[@]}")" sleep 0.2 click --window "$WID" 1 sleep 0.1
 xdotool mousemove --window "$WID" 750 790 sleep 0.2 click --window "$WID" 1 sleep 0.1
 xdotool mousemove --window "$WID" 20 20 sleep 0.2 click --window "$WID" --repeat 2 --delay 250 1 sleep 0.1
 reset
